378a708157473f157283b0d29a7a4dadd452bfc0
Salvatore Pulino <spspulino@gmail.com>
2024-10-17 19:18:23 +0200

    [dev] fix incidence controller

--- src/main/java/com/cesur/general/controllers/IncidenceController.java
+++ src/main/java/com/cesur/general/controllers/IncidenceController.java
@@ -1,24 +1,18 @@
 package com.cesur.general.controllers;
 
-import java.util.Date;
+
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.cesur.general.models.dtos.IncidenceDTO;
 import com.cesur.general.services.IncidenceService;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.websocket.server.PathParam;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
